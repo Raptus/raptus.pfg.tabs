95ec5b57bbf15033c4888668d9803ec1253e9dbf
Jonas Wagner <jwagner@raptus.com>
2012-11-02 19:00:19 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a2'
 setup(name='raptus.pfg.tabs',
       version=version,
       description="Provides a tabbed view of the fieldsets of a form created through PloneFormGen",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.pfg.tabs',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.pfg.tabs',
+      url='https://github.com/Raptus/raptus.pfg.tabs',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus.pfg'],
